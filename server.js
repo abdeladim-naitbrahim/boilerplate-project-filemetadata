540327a758f83d283b159db50b26f87d033b0aae
Abdeladim Nait Brahim <bnightn@gmail.com>
2021-01-15 22:34:33 +0000

    My solution

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var bodyparser=require('body-parser');
+var fileUpload=require('express-fileupload');
 var app = express();
-
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
+app.use(fileUpload({
+  limits: { fileSize: 50 * 1024 * 1024 },
+}));
+app.use(bodyparser.json());
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -14,7 +22,13 @@ app.get('/', function (req, res) {
 
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+
+
+
+app.post('/api/fileanalyse', function(req, res) {
+//saveuser( req.body.username,y=>res.json(y));
+let x=req.files.upfile;
+let y={name:x.name,type:x.mimetype,size:x.size}
+console.log(y);
+res.json(y);
 });
